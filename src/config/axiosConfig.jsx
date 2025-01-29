a621ad2a4453b073c868a0c94cdc228a975a8bfb
Calvin Oker <calvinoker8@gmail.com>
2025-01-29 15:23:44 +0300

    updating

--- src/config/axiosConfig.jsx
+++ src/config/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 const token = localStorage.getItem('token'); 
 
 const axiosInstance = axios.create({
-  baseURL: 'https://hms-mongodb.vercel.app',
+  baseURL: 'https://hms-mongodb-ubh1.vercel.app',
   // other config options if needed
   headers: { "x-access-token": token, "Content-Type": "application/json" },
 });
